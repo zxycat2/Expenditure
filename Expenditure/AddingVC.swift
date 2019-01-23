8620d77200709fb439dbc5c0cb7667b616005453
周熙岩 <zxycat2@zhouxiyandemacbook-pro.local>
2019-01-23 00:27:16 +0000

    Tep

--- Expenditure/AddingVC.swift
+++ Expenditure/AddingVC.swift
@@ -10,15 +10,38 @@ import UIKit
 
 
 
-class AddingVC: UIViewController{
+class AddingVC: UIViewController, UITextFieldDelegate{
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        //设置监视键盘位置
+        NotificationCenter.default.addObserver(self,selector:#selector(self.keyboardPositionDidChange(_:)),name:UIResponder.keyboardWillChangeFrameNotification, object: nil)
+        
+        
+        
         
         // Do any additional setup after loading the view.
     }
     
+
+    //在监视到键盘变化后call的函数，把textView上移
+    @objc func keyboardPositionDidChange(_ notification:Notification){
+        let info  = notification.userInfo
+        let keyboardRect = (info?[UIResponder.keyboardFrameEndUserInfoKey] as! NSValue).cgRectValue
+        let offSetY = keyboardRect.origin.y - UIScreen.main.bounds.height
+        UIView.animate(withDuration: 0.3, animations: {
+            self.textField.transform = CGAffineTransform(translationX: 0, y: offSetY)
+            }, completion: nil)
+    }
     
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        self.textField.resignFirstResponder()
+        return true
+    }
+    
+   
+    
+    @IBOutlet weak var textField: UITextField!
     
     
     
